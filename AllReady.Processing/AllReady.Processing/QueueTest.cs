f21a7c6d2b16b7f7520b2f974cef749da5c7ccf7
James Chambers <james@jameschambers.com>
2018-01-31 22:04:15 -0600

    Initial Commit
    - created solution and project
    - added test queue processing function
    -

---
+++ AllReady.Processing/AllReady.Processing/QueueTest.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace AllReady.Processing
+{
+    public static class QueueTest
+    {
+        // This function can be used locally to test interaction with your 
+        // local storage emulator.
+
+        [FunctionName("QueueTest")]
+        public static void Run([QueueTrigger("queue-test", Connection = "")]string item, TraceWriter log)
+        {            
+            log.Info($"A message was dequeued from the queue-test queue: {item}");
+        }
+    }
+}
