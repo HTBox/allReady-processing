164e2204c6da234998a071c5648b9bccde96afc5
Derek Christensen <derek@heavyd.net>
2018-03-02 20:38:17 -0700

    #3 Process Uploaded Images

---
+++ AllReady.Processing.UnitTest/ImageHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Shouldly;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+
+namespace AllReady.Processing.UnitTest
+{
+    public static class ImageHelpers
+    {
+        public static Stream GetImageStream(int width, int height, IImageFormat format = null)
+        {
+            var stream = new MemoryStream();
+            using (var img = new Image<Rgba32>(width, height))
+            {
+                img.Save(stream, format ?? ImageFormats.Png);
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+            return stream;
+        }
+
+        public static void ShouldBeImageWithDimensions(this Stream stream, int width, int height)
+        {
+            using (var img = Image.Load<Rgba32>(stream))
+            {
+                img.Width.ShouldBe(width);
+                img.Height.ShouldBe(height);
+            }
+        }
+    }
+}
