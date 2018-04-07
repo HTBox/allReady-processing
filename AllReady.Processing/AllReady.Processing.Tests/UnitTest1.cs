285fe25dd0087553167f02003da0bc0e8ae55d0c
Georgi Parlakov <gparlakov@gmail.com>
2018-04-07 18:07:38 +0300

    Add the cibling project - works

--- AllReady.Processing/AllReady.Processing.Tests/UnitTest1.cs
+++ AllReady.Processing/AllReady.Processing.Tests/UnitTest1.cs
@@ -17,10 +17,10 @@ namespace AllReady.Processing.Tests
             // Arrange
             //var loggerMock = new Mock<TraceWriter>();
             //loggerMock.Setup(l => l.Info(It.IsAny<string>(), It.IsAny<string>()));
-            ////    var expected = new Mail { From = new Email(EmailSentFrom, "AllReady"), Subject = "test" };
-            //    var personalization = new Personalization();
-            //    personalization.AddTo(new Email("test@gmail.com"));
-            //    expected.AddPersonalization(personalization);
+            //var expected = new Mail { From = new Email(EmailSentFrom, "AllReady"), Subject = "test" };
+            //var personalization = new Personalization();
+            //personalization.AddTo(new Email("test@gmail.com"));
+            //expected.AddPersonalization(personalization);
 
             //    var incomingMessage = new QueuedEmailMessage { Subject = "test", Recipient = "test@gmail.com", Message = "simple text message" };
             //    // Act
@@ -43,12 +43,22 @@ namespace AllReady.Processing.Tests
 
             Assert.Equal("test", input);
             mock.Object.Foo().ShouldBe("fooReturn");
-            
+
         }
 
+        [Fact]
+        public void TestCiblingProject()
+        {
+            // Arrange
+            var t = new QueuedEmailMessage { Subject = "Test" };
+
+            // Assert
+            t.Subject.ShouldBe("Test");
+        }
         public interface IMockable
         {
             string Foo();
         }
+
     }
 }
