90d96b651ef61e7073831d5345363b3fca8efc36
Georgi Parlakov <gparlakov@gmail.com>
2018-04-07 13:51:55 +0300

    Copy over the code from original repo(allready) Add the necessary packages Todo add sedn grid auth

---
+++ AllReady.Processing/AllReady.Processing/QueuedEmailMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace AllReady.Processing
+{
+
+    public class QueuedEmailMessage
+    {
+
+        public string Recipient { get; set; }
+
+        public string Message { get; set; }
+
+        public string HtmlMessage { get; set; }
+
+        public string Subject { get; set; }
+    }
+}
