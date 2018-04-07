d6c90b6696601889e0732cc289f4adf5cba57bb3
Georgi Parlakov <gparlakov@gmail.com>
2018-04-07 18:04:04 +0300

    add Moq - works

--- AllReady.Processing/AllReady.Processing.Tests/UnitTest1.cs
+++ AllReady.Processing/AllReady.Processing.Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿//using Moq;
+﻿using Moq;
 using Xunit;
 using Shouldly;
 //using Microsoft.Azure.WebJobs.Host;
@@ -11,32 +11,44 @@ namespace AllReady.Processing.Tests
         // comes from Environment Variable
         const string EmailSentFrom = "from@tests.com";
 
-        //[Fact]
-        //public void TestMethod1()
-        //{
-        //    // Arrange
-        //    var loggerMock = new Mock<TraceWriter>();
-        //    loggerMock.Setup(l => l.Info(It.IsAny<string>(), It.IsAny<string>()));
-        //    var expected = new Mail { From = new Email(EmailSentFrom, "AllReady"), Subject = "test" };
-        //    var personalization = new Personalization();
-        //    personalization.AddTo(new Email("test@gmail.com"));
-        //    expected.AddPersonalization(personalization);
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            //var loggerMock = new Mock<TraceWriter>();
+            //loggerMock.Setup(l => l.Info(It.IsAny<string>(), It.IsAny<string>()));
+            ////    var expected = new Mail { From = new Email(EmailSentFrom, "AllReady"), Subject = "test" };
+            //    var personalization = new Personalization();
+            //    personalization.AddTo(new Email("test@gmail.com"));
+            //    expected.AddPersonalization(personalization);
 
-        //    var incomingMessage = new QueuedEmailMessage { Subject = "test", Recipient = "test@gmail.com", Message = "simple text message" };
-        //    // Act
-        //    ProcessEmailQueueMessage.Run("testItem", out Mail message, loggerMock.Object);
-        //    // Assert
-        //    message.ShouldBe(expected);
-        //}
+            //    var incomingMessage = new QueuedEmailMessage { Subject = "test", Recipient = "test@gmail.com", Message = "simple text message" };
+            //    // Act
+            //    ProcessEmailQueueMessage.Run("testItem", out Mail message, loggerMock.Object);
+            //    // Assert
+            //    message.ShouldBe(expected);
+        }
 
         [Theory]
         [InlineData("test")]
         public void TestMethod2(string input)
         {
+            // Arrange
+            var mock = new Mock<IMockable>();
+            mock.Setup(i => i.Foo()).Returns("fooReturn");
+
+            // Assert
             input.ShouldBe("test");
             input.ShouldNotBe("not test");
 
             Assert.Equal("test", input);
+            mock.Object.Foo().ShouldBe("fooReturn");
+            
+        }
+
+        public interface IMockable
+        {
+            string Foo();
         }
     }
 }
